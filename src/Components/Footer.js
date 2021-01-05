12ab7315e6ce8a9480ac440ca26386eafae7a27d
Lesuz <50194165+Lesuz@users.noreply.github.com>
2021-01-05 19:31:05 +0200

    fixed copyright year

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
                 <a href="https://github.com/Lesuz?tab=repositories" target="_blank" rel="noreferrer">GitHub</a>
                 <a href="https://www.linkedin.com/in/susanna-h%C3%A4m%C3%A4l%C3%A4inen-81557616b/" target="_blank" rel="noreferrer">LinkedIn</a>
             </div>
-            &copy; Copyright 2020 Susanna Hämäläinen
+            &copy; Copyright 2021 Susanna Hämäläinen
         </footer>
     )
 }
